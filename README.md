7c40787142587582380fd1f6f345fc842294b2ea
Emil Farisan <emilfs@Emils-MacBook-Pro.local>
2020-07-26 10:01:21 +0700

    added requirements.txt and update the readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mdn-crawler
-Crawler that is used for the migration of MDN Web Docs to Firefox Source Docs
+mdn-crawler is a Crawler that is used for the migration of MDN Web Docs to Firefox Source Docs
+
+```sh
+$ cd crawler
+$ pip install -r requirements.txt
+```
+To run the general crawler (still needs work)
+```sh
+$ scrapy crawl mdn
+```
+To run the csv crawler
+```sh
+$ scrapy crawl csv
+```
--- crawler/crawler/spiders/mdn_spider.py
+++ crawler/crawler/spiders/mdn_spider.py
@@ -1,11 +1,11 @@
 import os
 import logging
+import w3lib.html
+import pypandoc
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-import w3lib.html
 
-import pypandoc
 
 logger = logging.getLogger()
 
@@ -31,21 +31,32 @@ class MdnSpider(CrawlSpider):
             f.write(response.body)
 
     def convert_to_rst(self, response, filename):
+        """
+        edit the which_ones to know which is going to be deleted
+
+        To test:
+        - put div on the 3rd position
+        - determine what to use from the combination of span, section, button (the lesser the better)
+        """
         clean_resp = w3lib.html.remove_tags_with_content(response.text, which_ones=('script', 'head', 'header', 'footer', 'span', 'section', 'button', 'div',))
         rst_out = pypandoc.convert_text(clean_resp, 'rst', format='html')
 
         # with open(filename, 'w') as f:
         #     f.write(rst_out)
+        """
+        delete the comment above and use it instead of the bottom one
+        pros = the first 3 line is gone
+        cons = need more resource, could cause unwanted side effects since we are using writelines instead of write
+        """
         data = rst_out.splitlines(True)
         with open(filename, 'w') as f:
             f.writelines(data[4:])
 
 
     def parse_item(self, response):
-        # title = response.css('div.titlebar h1.title::text').get()
         title = response.xpath('//title/text()').extract_first()
-        filename = 'htmls/%s.rst' % title #response.url.rsplit('/', 1)[-1]
-        os.makedirs("htmls/", exist_ok=True)
+        filename = 'rst/%s.rst' % title #response.url.rsplit('/', 1)[-1]
+        os.makedirs("rst/", exist_ok=True)
 
         if self.is_mdn(response):
             # self.save_to_html(response, filename)
